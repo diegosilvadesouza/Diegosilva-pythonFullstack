359db94bf8f9c278c4bed78e438989fa0b47c568
diegosilvadesouza <aprendendoserdesenvolvedor@gmail.com>
2025-08-17 18:21:50 -0300

    trabalho atrasado

--- aula.4/index.js
+++
@@ -1,6 +0,0 @@
-// Cálculo do preço final com desconto
-let preco = 150.0;
-let valorDesconto = 20;
-let precoFinal = preco - valorDesconto;
-console.log("Preço final após desconto: R$" + precoFinal);
-
