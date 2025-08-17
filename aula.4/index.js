e260bfe6aa8bd9acf02b3669652bdaf28c63393f
diegosilvadesouza <aprendendoserdesenvolvedor@gmail.com>
2025-08-17 12:09:09 -0300

    mudando pasta de local

---
+++ aula.4/index.js
@@ -0,0 +1,6 @@
+// Cálculo do preço final com desconto
+let preco = 150.0;
+let valorDesconto = 20;
+let precoFinal = preco - valorDesconto;
+console.log("Preço final após desconto: R$" + precoFinal);
+
